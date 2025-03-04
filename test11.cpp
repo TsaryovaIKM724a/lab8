3daf8a0658d65d90c75e7a64e5a3116f48465678
TsaryovaIKM724a <Mariia.Tsarova@infiz.khpi.edu.ua>
2025-03-04 16:17:10 +0200

    Update test11.cpp

--- test11.cpp
+++ test11.cpp
@@ -6,22 +6,31 @@ int main() {
     int chislo;
     cout << "Введите число: ";
     cin >> chislo;
+    
+    int prostyeDeliteli[100];
+    int count = 0;
+    
     cout << "Простые числа среди делителей: ";
-
+    
     for (int i = 1; i <= chislo; i++) {
         if (chislo % i == 0) {
             bool naturalChislo = true;
             if (i < 2) naturalChislo = false;
-
+            
             for (int j = 2; j * j <= i; j++) {
                 if (i % j == 0) {
                     naturalChislo = false;
                     break;
                 }
             }
-
+            
             if (naturalChislo) {
                 cout << i << " ";
+                prostyeDeliteli[count] = i;
+                count++;
+            }
+            for (int i = 0; i < count; i++) {
+                cout << prostyeDeliteli[i] << " ";
             }
         }
     }
